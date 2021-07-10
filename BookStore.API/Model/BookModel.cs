61cd63b1de85792a583b5ce2b59ba2d28cd6798a
mmraihan <mm_raihan@live.com>
2021-07-10 11:28:35 +0600

    Implement Model validations in Web APIs

--- BookStore.API/Model/BookModel.cs
+++ BookStore.API/Model/BookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,8 @@ namespace BookStore.API.Model
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="Please add Book's Title")]
         public string Title { get; set; }
-
         public string Description { get; set; }
     }
 }
