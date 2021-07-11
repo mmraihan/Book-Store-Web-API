0c38c3f2023ec7be8458cd38d13e2cab916a7906
mmraihan <mm_raihan@live.com>
2021-07-11 21:45:52 +0600

    Create Controller and Repository for Login and Signup

---
+++ BookStore.API/Repository/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Repository
+{
+    public class AccountRepository: IAccountRepository
+    {
+
+        public AccountRepository()
+        {
+
+        }
+
+
+    }
+}
